8729b7e5c770b67d941960b692ccb6e3c489a642
Damian <58256720+Damian-crypto@users.noreply.github.com>
2021-07-02 10:02:26 +0530

    Create cpu.c

---
+++ cpu.c
@@ -0,0 +1,16 @@
+#ifndef PROCESSOR_H
+#define PROCESSOR_H
+
+class controls
+{
+private:
+  unsigned int cpu_log;
+  
+public:
+  void start_controller(unsigned int flag)
+  {
+    cpu_log |= flag;
+  }
+};
+
+#endif
